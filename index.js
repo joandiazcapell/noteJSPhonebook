b8fc3d7b37f3f421bc429d5004aa44e2b5faa369
Joan Diaz <piasbcn@gmail.com>
2024-08-29 09:50:06 +0200

    adding frontend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', (req) => {
@@ -101,7 +104,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${numerPeople} people</p><p>${new Date()}</p>`)
 })
   
-  const PORT = 3001
+const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
