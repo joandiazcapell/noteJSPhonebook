df70a6d9c5eed82cb97380725289c8087c3ec79a
Joan Diaz <piasbcn@gmail.com>
2024-08-29 10:05:47 +0200

    Change orders of express midlewares

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.static('dist'))
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
     if (req.method === 'POST') {
