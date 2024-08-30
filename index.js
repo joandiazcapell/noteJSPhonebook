4ad3a7707b5c0c8589d5b84a79785c779767562b
Joan Diaz <piasbcn@gmail.com>
2024-08-30 17:34:53 +0200

    Middleware and error handling

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const requestLogger = require('./middlewares/requestLogger');
 const Person = require('./models/person')
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
 
 morgan.token('body', (req) => {
     if (req.method === 'POST') {
@@ -30,10 +33,15 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -41,9 +49,10 @@ app.delete('/api/persons/:id', (request, response) => {
     .then(result => {
         response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const {name, number } = request.body;
   Person.findByIdAndUpdate(
     request.params.id, 
@@ -53,6 +62,7 @@ app.put('/api/persons/:id', (request, response) => {
   .then(updatedPerson => {
     response.json(updatedPerson)
   })
+  .catch(error => next(error))
 })
 
 /*
@@ -81,9 +91,30 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
     numerPeople = persons.length;
     response.send(`<p>Phonebook has info for ${numerPeople} people</p><p>${new Date()}</p>`)
+  })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
   
 const PORT = process.env.PORT
   app.listen(PORT, () => {
